c2d1617f2945bcc08119064b5dfbd0e1b501d416
Eva Maria <evamariacamacho@gmail.com>
2021-09-24 14:58:45 +0200

    commint -m "parallax"

---
+++ includes/parallax/parallax_content.php
@@ -0,0 +1,21 @@
+<div class="parallax-content baner-content" id="home">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+
+
+                </div>
+                <div class="col-sm-6">
+                     <div class="text-content">
+                        <h2><em>Proyecto</em> <span>Acciones</span>Inversiones</h2>
+                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reprehenderit quidem veniam porro beatae error odio.</p>
+                        <div class="primary-white-button">
+                             <a href="#" class="scroll-link" data-id="about">Empezamos</a>
+                         </div>
+
+                    </div>
+                </div>
+           
+            </div>
+        </div>
+    </div>
